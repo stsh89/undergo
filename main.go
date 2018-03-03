a9d3fba45839600cde298487e8e4eaa057d278db
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-03-03 20:50:13 +0200

    Bump response version

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	port := os.Getenv("PORT")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "{\"payload\": {\"status\":\"success\", \"version\": 1}}")
+		io.WriteString(w, "{\"payload\": {\"status\":\"success\", \"version\": 2}}")
 	})
 
 	http.ListenAndServe(":" + port, nil)
