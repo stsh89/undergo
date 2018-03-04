9ed3cc073136f8bd90bc67c6993de4bfa672fdb9
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-03-04 19:41:31 +0200

    Adjust health check response

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,5 +14,5 @@ func root() {
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, `{"payload":{"status":"success","version":4}}`)
+	io.WriteString(w, `{"version":5}`)
 }
--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -22,7 +22,7 @@ func TestHealthCheckHandler(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	expected := `{"payload":{"status":"success","version":4}}`
+	expected := `{"version":5}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
